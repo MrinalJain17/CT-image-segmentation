Computed normalization stats for stacked windowed transform and some minor fixes

capstone/data/datasets.py
@@ -7,41 +7,36 @@ from capstone.utils import miccai
 from torch.utils.data import Dataset
 
 
-class _MiccaiDataset2D(Dataset):
-    def __init__(self, path: str) -> None:
+class MiccaiDataset2D(Dataset):
+    """TODO
+
+    """
+
+    def __init__(self, path: str, structure: str = None, transform=None) -> None:
         self.path = Path(path).absolute()
+        self.structure_required = structure
+        self.transform = transform
 
         self.instance_paths = []
         for instance in self.path.iterdir():
             self.instance_paths.append(instance.as_posix())
         self.instance_paths.sort()  # To get same order on Windows and Linux (cluster)
 
+        if self.structure_required is not None:
+            assert (
+                self.structure_required in miccai.STRUCTURES
+            ), "Invalid structure name passed"
+
     def __len__(self) -> int:
         return len(self.instance_paths)
-
-    def __getitem__(self, index: int):
-        raise NotImplementedError()
-
-
-class MiccaiDataset2D(_MiccaiDataset2D):
-    """TODO
-
-    """
-
-    def __init__(self, path: str, structure: str = None, transform=None) -> None:
-        super(MiccaiDataset2D, self).__init__(path)
-        if structure is not None:
-            assert structure in miccai.STRUCTURES, "Invalid structure name"
-        self.structure_required = structure
-        self.transform = transform
 
     def __getitem__(self, index: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         instance = np.load(self.instance_paths[index])
         image = np.transpose(instance["image"], (1, 2, 0))
         masks, mask_indicator = instance["masks"], instance["mask_indicator"]
 
-        assert len(mask_indicator) == 9
-        assert masks.shape[0] == 9
+        assert len(mask_indicator) == len(miccai.STRUCTURES)
+        assert masks.shape[0] == len(miccai.STRUCTURES)
 
         masks = list(masks)
 
@@ -52,8 +47,10 @@ class MiccaiDataset2D(_MiccaiDataset2D):
 
         if self.structure_required is not None:
             idx = miccai.STRUCTURES.index(self.structure_required)
-            masks = masks[idx]
-            mask_indicator = mask_indicator[idx]
+            masks = np.expand_dims(masks[idx], axis=0)
+            mask_indicator = np.expand_dims(mask_indicator[idx], axis=0)
+        else:
+            masks = np.stack(masks)
 
         return image, masks, mask_indicator
 