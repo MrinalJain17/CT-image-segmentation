12fa88caa0abd9d9bbfbeea77a27b90fe8c048d1
Mrinal Jain <mrinaljain007@gmail.com>
2020-10-24 20:28:29 -0400

    Updates to lightning model and training - losses, visualization, etc

--- capstone/models/__init__.py
+++ capstone/models/__init__.py
@@ -1,1 +1,4 @@
+from capstone.models.losses import BCELossWrapper, DiceLossWrapper
 from monai.networks.nets import UNet
+
+LOSSES = {"BCELoss": BCELossWrapper(), "DiceLoss": DiceLossWrapper()}
--- capstone/models/losses.py
+++ capstone/models/losses.py
@@ -1,18 +1,58 @@
-import torch
+from functools import partial
+
+import torch.nn as nn
 import torch.nn.functional as F
+from monai.losses.dice import DiceLoss
 
 
-def binary_cross_entropy_with_logits(input, target, mask_indicator, **kwargs):
-    loss_per_class = F.binary_cross_entropy_with_logits(
-        input, target, reduction="none", **kwargs
-    ).mean(
-        dim=(2, 3)
-    )  # Shape: (N, C) where C is the number of segmentation classes
+class BaseLossWrapper(nn.Module):
+    def __init__(self, apply_mask=True):
+        super(BaseLossWrapper, self).__init__()
+        self.loss_fx = None  # Required to be implemented in the subclasses
+        self.apply_mask = apply_mask
 
-    annotations_per_image = (
-        mask_indicator.sum(dim=1) if mask_indicator.shape[1] != 1 else None
-    )
-    effective_loss = torch.einsum("nc,nc->n", loss_per_class, mask_indicator)
-    if annotations_per_image is not None:  # Multiple classes
-        effective_loss /= annotations_per_image
-    return effective_loss.mean()
+    def _compute_loss_per_mask(self, input, target):
+        raise NotImplementedError()
+
+    def forward(self, input, target, mask_indicator=None):
+        """
+        input, target - Shape: (N, C, H, W)
+        mask_indicator - Shape: (N, C)
+        """
+        loss_per_mask = self._compute_loss_per_mask(input, target)  # Shape: (N, C)
+
+        if self.apply_mask:
+            assert mask_indicator is not None, "Mask indicators not provided"
+            return _batch_masked_mean(loss_per_mask, mask_indicator)  # Scalar
+
+        return loss_per_mask  # Shape: (N, C)
+
+
+class BCELossWrapper(BaseLossWrapper):
+    def __init__(self, apply_mask=True, **kwargs):
+        super(BCELossWrapper, self).__init__(apply_mask=apply_mask)
+        self.loss_fx = partial(
+            F.binary_cross_entropy_with_logits, reduction="none", **kwargs
+        )
+
+    def _compute_loss_per_mask(self, input, target):
+        return self.loss_fx(input, target).mean(dim=(2, 3))  # Shape: (N, C)
+
+
+class DiceLossWrapper(BaseLossWrapper):
+    def __init__(self, apply_mask=True):
+        super(DiceLossWrapper, self).__init__(apply_mask=apply_mask)
+        self.loss_fx = DiceLoss(sigmoid=True, reduction="none")
+
+    def _compute_loss_per_mask(self, input, target):
+        return self.loss_fx(input, target)  # Shape: (N, C)
+
+
+def _batch_masked_mean(array, mask):
+    """
+    array, mask - Shape: (N, C)
+    """
+    masked_values = array * mask
+    if mask.shape[-1] == 1:
+        return masked_values.mean()
+    return (masked_values.sum(axis=-1) / mask.sum(axis=-1)).mean()
