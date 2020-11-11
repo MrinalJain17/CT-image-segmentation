0d1ef1f7ca0889dd9de1a69226f02dc571c4d58d
Mrinal Jain <mrinaljain007@gmail.com>
2020-11-11 13:44:20 -0500

    Fix squashing

--- capstone/training/utils.py
+++ capstone/training/utils.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def _squash_masks(self, masks, n_classes, device):
+def _squash_masks(masks, n_classes, device):
     _temp = torch.arange(1, n_classes, device)
     masks = (masks * _temp[None, :, None, None]).max(dim=1).values
     return masks
