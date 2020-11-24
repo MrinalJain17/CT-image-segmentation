de1aa7731d8e1fe2a73064a3dc997e5232d8218f
Mrinal Jain <mrinaljain007@gmail.com>
2020-11-24 06:07:45 -0500

    Added code for mixup training

--- capstone/training/utils.py
+++ capstone/training/utils.py
@@ -1,9 +1,12 @@
 """
-These functions works from PyTorch 1.7 onwards. Will fail for previous
-versions due to changes in `max()` and `argmax()`
+The functions `_squash_masks()` and `_squash_predictions()` work from PyTorch 1.7
+onwards. Will fail for previous versions due to changes in `max()` and `argmax()`
 """
 
+import numpy as np
 import torch
+
+RNG = np.random.default_rng(seed=12342)
 
 
 def _squash_masks(masks, n_classes, device):
@@ -14,3 +17,18 @@ def _squash_masks(masks, n_classes, device):
 
 def _squash_predictions(preds):
     return torch.softmax(preds, dim=1).argmax(dim=1)  # Shape: (N, H, W)
+
+
+def mixup_data(images, alpha=0.2, device=None):
+    """Code adapted from: https://github.com/facebookresearch/mixup-cifar10"""
+    batch_size = images.shape[0]
+
+    lambda_ = RNG.beta(alpha, alpha)
+    index = torch.randperm(batch_size, device=device)
+    mixed_images = mixup_tensors(images, images[index], lambda_)
+
+    return mixed_images, index, lambda_
+
+
+def mixup_tensors(tensor_1, tensor_2, lambda_):
+    return lambda_ * tensor_1 + (1 - lambda_) * tensor_2
